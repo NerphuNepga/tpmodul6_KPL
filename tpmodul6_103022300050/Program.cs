f2402effa57bbf975685aeb4cf6f98beff4c680a
M Arrayan <fighterfly9@gmail.com>
2025-03-23 12:40:24 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300050/Program.cs
+++ tpmodul6_103022300050/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello KPL");
+using tpmodul6_103022300050;
+
+SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Muhammad Arrayan Fikri");
+video.IncreasePlayCount(10);
+video.PrintVideoDetails();
---
+++ tpmodul6_103022300050/SayaTubeVideo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_103022300050
+{
+    class SayaTubeVideo
+    {
+        private int id, playCount;
+        private string title;
+        private static Random random = new Random();
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            this.id = random.Next(10000, 99999);
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount += playCount;
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID         : " + this.id);
+            Console.WriteLine("Video Title      : " + this.title);
+            Console.WriteLine("Video Play Count : " + this.playCount);
+        }
+    }
+}
